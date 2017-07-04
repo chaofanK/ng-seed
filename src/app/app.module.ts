3e2f1b52b8f055c4aa94279774f8fae8fe480fbc
chaofank <chaofank@qq.com>
2017-07-04 22:41:17 +0800

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AppMaterialModule } from './app.material.module';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DummyComponent } from './dummy/dummy.component';
+
+export const ROUTES = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dummy', component: DummyComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DummyComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(ROUTES),
     AppMaterialModule
   ],
   providers: [],
